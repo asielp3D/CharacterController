7365d4b77190becbe82732e40a19d54792a769b1
asielp3D <asielmasespacio@gmail.com>
2023-11-17 10:57:15 +0100

    interactuar con cajas y tirarlas

--- Assets/Scripts/TPSController.cs
+++ Assets/Scripts/TPSController.cs
@@ -15,8 +15,15 @@ public class TPSController : MonoBehaviour
 
     [SerializeField] private float _jumpHeight = 1;
 
+    [SerializeField] private float _pushForce = 5;
+
+     [SerializeField] private float _throwForce = 10;
+
     private float _gravity = -9.81f;
+
     private Vector3 _playerGravity;
+
+    private bool _isAiming = false;
 
 
 
@@ -34,6 +41,12 @@ public class TPSController : MonoBehaviour
     [SerializeField] private LayerMask _groundLayer;
 
     private bool _isGrounded;
+
+    //Variables para coger cosas
+    public GameObject objectToGrab;
+
+    private GameObject grabedObject;
+    [SerializeField] private Transform _interactionZone;
 
     private Transform _camera;
     private Animator _animator;
@@ -55,10 +68,12 @@ public class TPSController : MonoBehaviour
         if(Input.GetButton("Fire2"))
         {
             AimMovement();
+            _isAiming = true;
         }
         else
         {
             Movement();
+            _isAiming = false;
         }
        
         
@@ -68,6 +83,16 @@ public class TPSController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.K))
         {
             RayTest();
+        }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            GrabObject();
+        }
+
+        if(Input.GetButtonDown("Fire1") && grabedObject != null && _isAiming)
+        {
+            ThrowObject();
         }
     }
 
@@ -170,5 +195,57 @@ public class TPSController : MonoBehaviour
                 caja.TakeDamage(shootDamage);
             }
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(_sensorPosition.position,_sensorRadius);
+    }
+
+    void OnControllerColliderHit (ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        if(body == null || body.isKinematic)
+        {
+            return;
+        }
+
+        if(hit.moveDirection.y < -0.2f)
+        {
+            return;
+        }
+
+        Vector3 pushDirection = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+        body.velocity = pushDirection * _pushForce / body.mass;
+    }
+
+    void GrabObject()
+    {
+        if(objectToGrab != null && grabedObject == null)
+        {
+            grabedObject = objectToGrab;
+            grabedObject.transform.SetParent(_interactionZone);
+            grabedObject.transform.position = _interactionZone.position;
+            grabedObject.GetComponent<Rigidbody>().isKinematic = true; // para que dejen de afectar las fisicas al objeto
+        }
+        else if(grabedObject != null)
+        {
+            grabedObject.GetComponent<Rigidbody>().isKinematic = false;
+            grabedObject.transform.SetParent(null);
+            grabedObject = null;
+        }
+    }
+
+    void ThrowObject()
+    {
+        Rigidbody grabedBody = grabedObject.GetComponent<Rigidbody>();
+
+        grabedObject.GetComponent<Rigidbody>().isKinematic = false;
+        grabedObject.transform.SetParent(null);
+        grabedObject.GetComponent<Rigidbody>().AddForce(_camera.transform.forward * _throwForce, ForceMode.Impulse);
+        grabedObject = null;
     }
 }
